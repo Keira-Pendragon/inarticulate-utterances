0a2fb078e4867a55777408298d7b6e0f781b48c8
Keira <98257645+Keira-Pendragon@users.noreply.github.com>
2023-01-23 18:50:32 -0800

    Update Number.java
    Pattern stuff idk

--- Number.java
+++ Number.java
@@ -7,10 +7,20 @@
  */
 public class Number extends Words
 {
+
+    private static final int DIGITVALUE = 0;
+    private static final int DIGITCLASS = 1;
+    private static final int DIGITMAGNITUDE = 3;
+    private static final int DIGITDELIMITER = 4;
+    
+    private static final int[] digit_pattern = {DIGITVALUE, DIGITCLASS, DIGITMAGNITUDE, DIGITDELIMITER};
+    
+    
     public Number()
     {
         
     }
+    
     public static Word detailWord(int type)
     {
         boolean random = randomWord();
