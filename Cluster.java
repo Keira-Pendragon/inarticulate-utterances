739efb5b98c4569c34fb3bca4b381fd15bedc69d
Keira <98257645+Keira-Pendragon@users.noreply.github.com>
2023-01-23 18:24:53 -0800

    Update Cluster.java
    Added dummy cluster arrays.

--- Cluster.java
+++ Cluster.java
@@ -7,6 +7,27 @@
 
 public class Cluster
 {
+    private static final String[] os_cluster = {"", ""};
+    private static final String[] om_cluster = {"", ""};
+    private static final String[] ol_cluster = {"", ""};
+    private static final String[][] Onset_Cluster = {os_cluster, om_cluster, ol_cluster};
+    
+    
+    private static final String[] ms_cluster = {"", ""};
+    private static final String[] mm_cluster = {"", ""};
+    private static final String[] ml_cluster = {"", ""};
+    private static final String[][] MidConst_Cluster = {ms_cluster, mm_cluster, ml_cluster};
+    
+    private static final String[] cs_cluster = {"", ""};
+    private static final String[] cm_cluster = {"", ""};
+    private static final String[] cl_cluster = {"", ""};
+    private static final String[][] Coda_Cluster = {cs_cluster, cm_cluster, cl_cluster};
+    
+    private static final String[] vs_cluster = {"", ""};
+    private static final String[] vm_cluster = {"", ""};
+    private static final String[] vl_cluster = {"", ""};
+    private static final String[][] Vowel_Cluster = {vs_cluster, vm_cluster, vl_cluster};
+    
     public Cluster()
     {
 
