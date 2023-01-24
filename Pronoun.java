0c7ed7fc39e4d1fe9085a86ecaeb4db731031599
Keira <98257645+Keira-Pendragon@users.noreply.github.com>
2023-01-23 16:53:23 -0800

    Update Pronoun.java
    detailWord() 6/7

--- Pronoun.java
+++ Pronoun.java
@@ -1,5 +1,5 @@
 /**
- * .java
+ * Pronoun.java
  * Author: Keira Pendragon
  * Author Date: 1/22/2023
  * 
@@ -12,4 +12,19 @@ public class Pronoun extends Words
     {
         
     }
+    
+    public static Word detailWord(int type)
+    {
+        boolean random = randomWord();
+        int length = clusterLength();
+        int [] pattern = pronoun_pattern;
+        boolean singular = singularWord(); // TODO correct this for nouns...
+        int possessive = selectPossessiveness();
+        int genus = selectGenus();
+        int count = selectNounCount();
+        int role = selectRole();
+        Word aWord = new Word(type, random, length, pattern, singular, possessive, genus, count, role);
+        return aWord;
+     }
+    
 }
