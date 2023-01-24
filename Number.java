ac64e4d3065eff0f340bd91c2c8fa78b65b3a1c1
Keira <98257645+Keira-Pendragon@users.noreply.github.com>
2023-01-23 16:47:01 -0800

    Update Number.java
    detailWord() 5/7

--- Number.java
+++ Number.java
@@ -11,4 +11,13 @@ public class Number extends Words
     {
         
     }
+    public static Word detailWord(int type)
+    {
+        boolean random = randomWord();
+        int digits = digitCount();
+        String b12 = requestB12ToTranslate();
+        Word aWord = new Word(type, random, digits, b12);
+        return aWord;
+     }
+    
 }
