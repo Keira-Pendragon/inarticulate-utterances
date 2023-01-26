2cf3aa476537b74a53ecbe080da93678e364f9a7
Keira <98257645+Keira-Pendragon@users.noreply.github.com>
2023-01-25 16:00:40 -0800

    Update SentenceHelper.java
    Time to test it...

--- SentenceHelper.java
+++ SentenceHelper.java
@@ -6,8 +6,12 @@
  * 
  */
 
- public class SentenceHelper
+ public class SentenceHelper extends WordHelper
 {
+  private static final int[] sentence_count_range = {1, 20};
+  private static final int[] word_count_range = {1, 15};
+  private static final String sentence_count_prompt = "How many sentences of this type would you like?\n(limit 20)";
+  private static final String word_count_prompt = "How many words should be in the sentence?\n(limite 15)";
     public SentenceHelper()
     {
         
@@ -15,6 +19,41 @@
 
     public static void helperLoop()
     {
-
+      boolean onceMore = true;
+      int sentenceCount;
+      Sentence sayWhat;
+      int sentenceCount;
+      while(onceMore)
+      {
+       sayWhat = detailSentence();
+       sentenceCount = SUI.ValidateInt(sentence_count_range, sentence_count_prompt);
+       for(int i = 0; i < sentenceCount; i++)
+       {
+         SUI.displayTextLN(buildSentence(sayWhat));
+       }
+       SUI.displayTextLn("Sentence batch Complete.");
+       onceMore = SUI.ValidateAgreement("Would you like to build more sentences?\n1) yes\n2) No");
+      }
     }
+  
+  private static Sentence detailSentence()
+  {
+   int wordCount = SUI.ValidateInt(word_count_range, word_count_prompt);
+   Sentence sayWhat = new Sentence();
+   for(int i = 0; i < wordCount; i++)
+   {
+     sayWhat.addWord(detailWord());
+   }
+   return sayWhat;
+  }
+  
+  private static String buildSentence(Sentence sayWhat)
+  {
+  String sayThis = "";
+   for(int i = 0; i < sayWhat.wordCount(); i++)
+   {
+    sayThis += fetchWord(sayWhat.fetchWord(i)) + (i + 1 == sayWhat.wordCount()) ? " " : ".";
+   }
+   return sayThis;
+  }
 }
