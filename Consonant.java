cff7c6d4aabdc4794703e1a5d35f6985aa7e824c
Keira <98257645+Keira-Pendragon@users.noreply.github.com>
2023-01-29 02:20:50 +0000

    baseline consonant stuff

--- Consonant.java
+++ Consonant.java
@@ -1,4 +1,27 @@
 public class Consonant 
 {
+    private final String THEA = "\u03B8";
+    private final String ZHEA = "\u02E2";
+    private final String SHEA = "\u02E9";
+    private final String NGEA = "\u014B";
+    private final String[] all_consonant = {"n", "d", "t", "h", "r", "l", NGEA, "g", "k", "m", "b", "p", "f", "v", THEA, ZHEA, "z", SHEA, "s"};
+
+    public Consonant()
+    {
+
+    }
+    
+    public String generateOnset(int length)
+    {
+        return "";
+    }
+    public String generateMidWord(int length)
+    {
+        return "";
+    }
+    public String generateCoda(int length)
+    {
+        return "";
+    }
     
 }
